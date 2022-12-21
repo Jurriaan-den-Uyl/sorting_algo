a4c7ce97c2d671f4149f73b178d5b7450fa23cc3
Jurriaan-den-Uyl <119678622+Jurriaan-den-Uyl@users.noreply.github.com>
2022-12-21 15:05:44 +0100

    Initial commit

---
+++ Main.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 20 11:53:28 2022
+
+@author: Mehr
+
+inplace sorting tool (very slow)
+"""
+
+#importing
+import random
+import time
+import numpy as np
+
+#function to check the amount of time elapsed
+start_time = time.process_time()
+
+class Values():
+    def __init__(self):
+        self.reset()
+    
+    def reset(self):
+        #create a random list of numbers and converting it to a numpy array
+        self.list_length = 1000
+        self.random_list = random.sample(range(2*self.list_length), self.list_length)
+        self.random_array = np.asarray(self.random_list)
+        print("--- original list ---")
+        print(self.random_array)
+        
+class Sort():
+    def __init__(self):
+        pass
+
+    def inplacesort(self, array):
+        res = []
+        while True:
+            for idx in range(1, array.size):
+                if array[idx - 1] > array[idx]:
+                    array[idx - 1], array[idx] = array[idx], array[idx - 1]
+                    res.append(False)
+                else:
+                    res.append(True)
+            if all(res) == True:
+                break
+            res.clear()
+        return array
+
+class Main():
+    def __init__(self):
+        self.values = Values()
+        self.sort = Sort()
+        
+    def run(self):
+        self.sort.inplacesort(self.values.random_array)
+        print("--- sorted list ---")
+        print(self.values.random_array)
+
+
+main = Main()
+main.run()
+
+#second part of the time checking function
+print("--- %s seconds ---" % (time.process_time() - start_time))
